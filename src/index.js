2b76353d35ea78461f15f09a198ffce0a5309636
Yakov Limanskiy <limanskiy1989@gmail.com>
2017-09-03 20:37:10 +0300

    add iframe / play video

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import Searchbar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAqY3DjIjycOJV2k0bUOZAQVIOUwbD67Rs';
 
-const App = () => {
-  return (
-    <div>
-      <Searchbar />
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+      super(props);
+      this.state = { videos: [] };
+      YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.setState({ videos });
+      });
+  }
+
+  render(){
+    return (
+      <div>
+        <Searchbar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos= {this.state.videos} />
+      </div>
+    );
+  }
 }
 
 
